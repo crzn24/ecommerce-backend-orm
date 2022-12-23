a0c563e0bf185281b86701929b37bd239f0b7519
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-23 15:00:55 -0800

    Completes Tag model

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,16 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, // Manually define the primary key
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      // allowNull: false, README does not say to make NOT NULL
+    },
   },
   {
     sequelize,
