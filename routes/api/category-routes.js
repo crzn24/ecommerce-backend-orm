f0d41d9b9c9e295756df42b911b212bc0f98d7c9
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-23 20:34:47 -0800

    Starts category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,51 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const allCategoryData = await Category.findAll({
+      inlclude: [{model: Product}]
+    });
+    res.status(200).json(allCategoryData); // 200 status code means the request is successful
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const allCategoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+
+    if (!allCategoryData) {
+      res.status(404).json({ message: 'No category associated with that id!' }); // 400 status code means the server could not understand the request
+      return;
+    }
+
+    res.status(200).json(allCategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+
 });
 
 module.exports = router;
