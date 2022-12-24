b44668d503fa00ac8b433c61f8cea2d928afa2df
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-23 16:39:57 -0800

    Finishes associations in index.js

--- models/index.js
+++ models/index.js
@@ -4,18 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category (a product can only belong to one category ONE TO ONE)
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
-// Categories have many Products
+// Categories have many Products (a category can have multiple products ONE TO MANY)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  // onDelete: 'CASCADE',
+});
 
+// Products belongToMany Tags (through ProductTag) ---- products have multiple tags ONE TO MANY
+Product.belongsToMany(Tag, { through: ProductTag });
+// Tag.hasMany(Product, {
+//   foreignKey: 'tag_id',
+// })
 
-// Products belongToMany Tags (through ProductTag)
-
-
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) ---- tags have many products ONE TO MANY
+Tag.belongsToMany(Product, { through: ProductTag });
 
 
 module.exports = {
