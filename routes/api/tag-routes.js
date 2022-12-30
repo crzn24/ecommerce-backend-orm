16208971c02070e37d04e0e0e2e988b31074bf94
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-30 15:00:11 -0800

    Finishes get routes for tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,51 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const allTags = await Tag.findAll({
+      inlclude: [{model: Product}]
+    });
+    res.status(200).json(allTags); // 200 status code means the request is successful
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const allTags = await Tag.findByPk(req.params.id, {
+      include: [{model: Product, through: ProductTag}]
+    });
+
+    if (!allTags) {
+      res.status(404).json({ message: 'No tag associated with that id!' }); // 400 status code means the server could not understand the request
+      return;
+    }
+
+    res.status(200).json(allTags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+
 });
 
 module.exports = router;
